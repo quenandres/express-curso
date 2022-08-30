b1fef55f6970f89fa876951eab1edada06273499
Jhonatan Mejia <quenandres@gmail.com>
2022-08-29 19:22:15 -0500

    http vs express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const express = require('express');
+
+const app = express(); //Server
+
+app.get('/', (req, res) => {
+    res.sendFile('./static/index.html',{
+        root: __dirname
+    });
+});
+
+app.listen(3000);
+console.log('server on port 3000');
