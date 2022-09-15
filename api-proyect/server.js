39c436f9328750db120087899fd9dfaf0c50f88a
Jhonatan Mejia <quenandres@gmail.com>
2022-09-15 12:32:13 -0500

    REST API PUT, DELETE

--- api-proyect/server.js
+++ api-proyect/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json()); // Middleware para que interprete json
 
-const products = [
+let products = [
     {
         id: 1,
         name: "Laptop",
@@ -23,12 +23,37 @@ app.post('/products', (req, res) => {
     res.json(newProduct);
 });
 
-app.put('/products', (req, res) => {
-    res.send('actualizacion product');
+app.put('/products/:id', (req, res) => {
+    try {
+        const newData = req.body; // { name: '', price:'' }
+        const productFound = products.find(product => product.id === parseInt(req.params.id));
+        
+        if( !productFound ) {
+            throw new Error('Product not found');
+        }
+
+        products = products.map(product => product.id === parseInt(req.params.id) ? {...product, ...newData} : product);
+        console.log(products);
+        res.status(200).json({message: 'Updated product'});
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+    
 });
 
-app.delete('/products', (req, res) => {
-    res.send('Eliminando product');
+app.delete('/products/:id', (req, res) => {
+    try {
+        const productFound = products.find(product => product.id === parseInt(req.params.id));
+        
+        if( !productFound ) {
+            throw new Error('Product not found');
+        }
+
+        products  = products.filter(product => product.id !== parseInt(req.params.id));        
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
 });
 
 app.get('/products/:id', (req, res) => {
