eec521ca5dcbdb444d75de2b77a4da7d95002d8d
Jhonatan Mejia <quenandres@gmail.com>
2022-10-02 16:15:36 -0500

    secu

--- src/db.js
+++ src/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 async function connectDB() {    
     const connection = await mysql.createConnection({
         database: 'expressdb',
-        user: 'awj8ym9cv6wjnmcwost2',
-        host: 'us-east.connect.psdb.cloud',
-        password: 'pscale_pw_SwjF4iJ2zjVPSnue3TMeKVuvcxOuS8Hb402mgzoflb2',
+        user: '',
+        host: 'us-east.connect.cloud',
+        password: '',
         ssl: {
             rejectUnauthorized: false
         }
